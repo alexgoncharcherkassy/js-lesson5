cfd6834e418f4ca9ba896ec10f6fcac1bdf0faf6
Oleksandr Gonchar <cs210785gaa@gmail.com>
2016-11-22 09:44:26 +0200

    added js, css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
+var todo = document.getElementById("todo");
+var todoButton = document.getElementById("todo-button");
+var Item = {
+    field: '',
+    status: false,
 
+    setField: function (field) {
+        this.field = field;
+    },
+    setStatus: function (status) {
+        this.status = status;
+    },
+    getField: function () {
+        return this.field;
+    },
+    getStatus: function () {
+        return this.status;
+    }
+};
+var items = [];
+
+todo.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode == 13 && todo.value !== '') {
+        todoButton.click();
+    }
+});
+
+todoButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    item = Object.create(Item);
+    item.setField(todo.value);
+    item.setStatus(false);
+    items.push(item);
+    todo.value = '';
+    console.log(items);
+});
+
+function checkItems() {
+    if (items.length > 0) {
+        items.forEach(function (val, ind, arr) {
+
+        });
+    }
+}
